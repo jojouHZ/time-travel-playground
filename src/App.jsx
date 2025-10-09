50c3d27ad92bbb4edfde23c37fe7f51bc465657b
Roman Suleymanov <jojou112@gmail.com>
2025-10-09 15:37:11 +0300

    feat: implement loading snapshots from history

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,14 @@ const App = () => {
     };
   }, []);
 
+  /* 
+    "-1" - live code, 
+    "0" - last saved snapshot, 
+    "1" - second to last saved snapshot, 
+    ...  - etc.
+  */
+  const [currentIndex, setCurrentIndex] = useState(-1); 
+
   
   // Save a snapshot of the current code
   const saveSnapshot = async () => {
@@ -50,6 +58,19 @@ const App = () => {
       console.error("Error saving snapshot:", error);
     }
   };
+  
+  
+  // Temporary
+  const inputValue = document.getElementById("input-value");
+
+
+  // Load a snapshot by its index
+  const loadSnapshot = (index) => {
+    if (index >= 0 && index < history.legth) {
+      setCode(history[index].code);
+      setCurrentIndex(index);
+    }
+  }
 
   const handleEditorChange = (newValue = "") => {
     setCode(newValue);
@@ -73,6 +94,17 @@ const App = () => {
         loading={<div>Loading Editor...</div>}
       />
       <button onClick={saveSnapshot}>Save Snapshot</button>
+      <span>
+        <input id="input-value" 
+              type="text" 
+              inputmode="numeric" 
+              pattern="^(?:-1|0|[1-9]d*)$" 
+              placeholder="-1 or 0+">
+        </input>
+        <button onClick={loadSnapshot(inputValue)}>Load Snapshot</button>
+
+      </span>
+
       <div>
         <h2>History</h2>
         <pre>{JSON.stringify(history, null, 2)}</pre>
